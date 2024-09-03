67cd861a8ffe0b586e0fbdcdfd098abf39e24634
naufalluth <naufaldvf123@gmail.com>
2024-09-03 08:44:41 +0700

    adding header to page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <header className="bg-rose-50 w-80 h-10 text-rose-900 font-bold rounded-3xl mt-4">
             <nav className="flex justify-evenly items-center text-center py-2 font-lg">
                 <a href="/" className="nav-hover">Write</a>
-                <span className="text-xl font-mono font-semibold italic nav-hover">DearDiary</span>
+                <span className="text-xl font-mono font-semibold italic nav-hover"><a href="/">DearDiary</a></span>
                 <a href="/diary" className="nav-hover">MyDiary</a>
             </nav>
         </header >
