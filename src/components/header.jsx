0642bc817cf8b082bd90c8c77684c7a61d5519cb
naufalluth <naufaldvf123@gmail.com>
2024-09-07 19:29:07 +0700

    Changing Achor to Link in Header Component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
-
+import { Link } from 'next/link'
 
 export const Header = () => {
     return (
         <header className="bg-rose-50 w-80 h-10 text-rose-900 font-bold rounded-3xl mt-4">
             <nav className="flex justify-evenly items-center text-center py-2 font-lg">
-                <a href="/" className="nav-hover">Write</a>
-                <span className="text-xl font-mono font-semibold italic nav-hover"><a href="/">DearDiary</a></span>
+                <Link href="/" className="nav-hover">Write</Link>
+                <span className="text-xl font-mono font-semibold italic nav-hover"><Link href="/">DearDiary</Link></span>
                 <a href="/diary" className="nav-hover">MyDiary</a>
             </nav>
         </header >
